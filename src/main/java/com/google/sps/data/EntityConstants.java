6582d1604f8e96b51597b0edb258f78544dab07d
Taniecer <taniecer@gmail.com>
2020-07-13 21:51:41 +0000

    Change fields to final

--- src/main/java/com/google/sps/data/EntityConstants.java
+++ src/main/java/com/google/sps/data/EntityConstants.java
@@ -1,23 +1,30 @@
+package com.google.sps.data;
+
 import java.util.Date;
 
-public class EntityConstants {
-  static class AttendeeEntity {
-    public static String TABLE_NAME = "Attendee";
-    public static String SESSION_ID = "SessionId";
-    public static String SCREEN_NAME = "ScreenName";
-    public static String TIME_LAST_POLLED = "TimeLastPolled";
+/** Class that creates constants for tables. **/
+public final class EntityConstants {
+
+  /** Class that creates entity constants for the Attendee table. **/
+  final static class AttendeeEntity {
+    public final static String TABLE_NAME = "Attendee";
+    public final static String SESSION_ID = "SessionId";
+    public final static String SCREEN_NAME = "ScreenName";
+    public final static String TIME_LAST_POLLED = "TimeLastPolled";
   }
 
-  static class InstanceEntity {
-    public static String TABLE_NAME = "Instance";
-    public static String INSTANCE_NAME = "InstanceName";
-    public static String SESSION_ID = "SessionId";
+  /** Class that creates entity constants for the Instance table. **/
+  final static class InstanceEntity {
+    public final static String TABLE_NAME = "Instance";
+    public final static String INSTANCE_NAME = "InstanceName";
+    public final static String SESSION_ID = "SessionId";
   }
 
-  static class SessionEntity {
-    public static String TABLE_NAME = "Session";
-    public static String SESSION_ID = "SessionId";
-    public static String SCREEN_NAME_OF_CONTROLLER = "SreenNameOfController";
-    public static String IP_OF_VM = "IpOfVM";
+  /** Class that creates entity constants for the Session table. **/
+  final static class SessionEntity {
+    public final static String TABLE_NAME = "Session";
+    public final static String SESSION_ID = "SessionId";
+    public final static String SCREEN_NAME_OF_CONTROLLER = "ScreenNameOfController";
+    public final static String IP_OF_VM = "IpOfVM";
   }
 }
