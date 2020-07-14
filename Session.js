8ce10336abe9440e83beed5e890fbf82b4e1f2e5
jcul22 <jculbreath@google.com>
2020-07-14 22:21:35 +0000

    Fixes the classes into the correct form of constructors instead of them being miscellanious.

--- Session.js
+++ Session.js
@@ -1,7 +1,38 @@
-class Session {
-    String sessionId; 
-    String instanceIp;
-    String listOfAttendees;
+class Session { 
+// To be utilized when unwrapping JSON from the future servlets. Will be on the client side and be used as a buffer  
+// for client to server information needed for other components of Virtual Movie Night.
+ 
+    constructor(sessionId) {
+        this.sessionId = sessionId;
+    }
+    constructor(instanceIp) {
+        this.instanceIp = instanceIp;
+    } 
+    constructor(listOfAttendees){
+        this.listOfAttendees = listOfAttendees;
+    }
+    
+    constructor(controllerIp){
+        this.controllerIp = controllerIp;
+    }
+
 
     
+    get SessionId() {
+        return sessionId;
+    } 
+
+    get InstanceIp() {
+        return instnaceIp;
+    } 
+
+    get ControllerIp() {
+        return controllerIp; 
+    } 
+
+    get ListofAttendees() { 
+        return listOfAttendees;
+    } 
 }
+
+export { Session };
--- User.js
+++ User.js
@@ -1,7 +1,28 @@
+//The User class can be utililized to keep certain information of the user, a class for the user needed to be 
+// created in addition to the session class. The test for this JS class file utilizes Jest.IO
+
 class User {
-    String screenName;
-    Boolean controllerStatus;
-    String userIp; 
+    constructor(screenName){
+        this.screenName = screenName;
+    }
+    constructor(controllerStatus){
+        this.controllerStatus = controllerStatus;
+    }
+    constructor(userIp){
+        this.userIp = userIp;
+    } 
+
+    get screenName() {
+        return this.screenName;
+    }
+    
+    get controllerStatus() {
+        return this.controllerStatus;
+    }
+    
+    get UserIp() {
+        return this.userIp;
+    }
     
     
 } 
