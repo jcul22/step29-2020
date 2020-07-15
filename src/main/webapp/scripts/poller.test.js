30a2e5b456eb1b49c606e5d4a113d36143451688
ckdesir <christopher.desir01@gmail.com>
2020-07-14 20:01:24 -0400

    Adds updated test

---
+++ src/main/webapp/scripts/poller.test.js
@@ -0,0 +1,45 @@
+import { Poller } from './poller';
+
+jest.useFakeTimers();
+
+test('We can check if poll() is called correct amount of times + correct return value', () => {
+  const poll = new Poller(pollingFunction, 1000);
+  const pollSpy = jest.spyOn(poll, 'poll_');
+  const setTimeoutSpy = jest.spyOn(window, 'setTimeout');
+  const clearTimeoutSpy = jest.spyOn(window, 'clearTimeout');
+
+  poll.testOnlySetMaxPollAttempts(3);
+  poll.start();
+
+  jest.runAllTimers();
+
+  expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
+  expect(setTimeoutSpy).toHaveBeenCalledTimes(3);
+  expect(pollSpy).toHaveBeenCalledTimes(4);
+  expect(poll.getLastResult()).toBe(true);
+
+  pollSpy.mockRestore();
+  setTimeoutSpy.mockRestore();
+  clearTimeoutSpy.mockRestore();
+});
+
+test.only('Test to see if stop is working correctly!', () => {
+  const poll = new Poller(pollingFunction, 1000);
+  const pollSpy = jest.spyOn(poll, 'poll_');
+  const setTimeoutSpy = jest.spyOn(window, 'setTimeout');
+  const clearTimeoutSpy = jest.spyOn(window, 'clearTimeout');
+
+  poll.start();
+  poll.stop();
+
+  jest.runAllTimers();
+
+  expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
+  expect(setTimeoutSpy).toHaveBeenCalledTimes(10);
+  expect(pollSpy).toHaveBeenCalledTimes(11);
+  expect(poll.getLastResult()).toBe(true);
+});
+
+function pollingFunction() {
+  return true;
+}
