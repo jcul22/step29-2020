4af954c2516d094a65fca0d4d0791d06e6f7ae07
ckdesir <christopher.desir01@gmail.com>
2020-07-16 12:18:06 -0400

    Adds SessionCache test

--- src/main/webapp/scripts/sessioncache.test.js
+++ src/main/webapp/scripts/sessioncache.test.js
@@ -1,31 +1,90 @@
 import { SessionCache } from './sessioncache';
+import { Poller } from './poller';
+import fetch from 'jest-fetch-mock';
 
-test('We can check if correct errors are thrown - start', () => {
-      try {
-        const cache = new SessionCache();
-        cache.start();
-      } catch (e) {
-        expect(e.message).toBe('Unimplemented');
-      }
-    }
-);
+fetch.enableMocks();
+jest.setTimeout(40000);
 
-test('We can check if correct errors are thrown - stop', () => {
-  try {
-    const cache = new SessionCache();
+const pollSpy = jest.spyOn(Poller.prototype, 'poll_');
+const setTimeoutSpy = jest.spyOn(window, 'setTimeout');
+const clearTimeoutSpy = jest.spyOn(window, 'clearTimeout');
+const params =  new URLSearchParams('?session-id=EEEE7&name=chris');
+
+const expected = {
+  sessionID: 'EEEE7',
+  controller: 'chris',
+  listOfAttendes: ['chris', 'bryan']
+};
+
+afterEach(() => {    
+  jest.clearAllMocks();
+  fetch.resetMocks();
+});
+
+test('Test to see if stop is working correctly!', (done) => {
+  fetch.mockResponse(JSON.stringify(expected));
+
+  const cache = new SessionCache(params, 1000, 1000);
+  cache.start();
+
+  setTimeout(() => {
     cache.stop();
-  } catch (e) {
-    expect(e.message).toBe('Unimplemented');
-  }
-}
-);
+    expect(clearTimeoutSpy).toHaveBeenCalledTimes(2);
+    expect(setTimeoutSpy.mock.calls.length).toBeGreaterThan(40);
+    expect(pollSpy.mock.calls.length).toBeGreaterThan(20);
+    expect(cache.getSessionInformation()).toEqual(expected);
+    done();
+  }, 30000);
+});
 
-test('We can check if correct errors are thrown - getSessionInfo', () => {
-  try {
-    const cache = new SessionCache();
-    cache.getSessionInformation();
-  } catch (e) {
-    expect(e.message).toBe('Unimplemented');
-  }
-}
-);
+test('We can check if poll() is called correct amount' + 
+    'of times + correct return value', (done) => {
+      fetch.mockResponse(JSON.stringify(expected));
+
+      const cache = new SessionCache(params, 1000, 1000);
+      cache.start();
+
+      setTimeout(() => {
+        expect(clearTimeoutSpy).toHaveBeenCalledTimes(0);
+        expect(setTimeoutSpy.mock.calls.length).toBeGreaterThan(10);
+        expect(pollSpy.mock.calls.length).toBeGreaterThanOrEqual(5);
+        expect(cache.getSessionInformation()).toEqual(expected);
+        done();
+      }, 5000);
+});
+
+test('stopping before starting', () => {
+  fetch.mockResponse(JSON.stringify(expected));
+  const cache = new SessionCache(params, 1000, 1000);
+  cache.stop();
+  expect(clearTimeoutSpy).toHaveBeenCalledTimes(2);
+  expect(pollSpy).toHaveBeenCalledTimes(0);
+  expect(setTimeoutSpy).toHaveBeenCalledTimes(0);
+  expect(cache.getSessionInformation()).toBeFalsy();
+});
+
+test('starting up, immediately stopping', () => {
+  fetch.mockResponse(JSON.stringify(expected));
+  const cache = new SessionCache(params, 1000, 1000);
+  cache.start();
+  cache.stop();
+  expect(clearTimeoutSpy).toHaveBeenCalledTimes(2);
+  expect(pollSpy).toHaveBeenCalledTimes(1);
+  expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
+  expect(cache.getSessionInformation()).toBeFalsy();
+});
+
+test('starting up after stopping', (done) => {
+  fetch.mockResponse(JSON.stringify(expected));
+  const cache = new SessionCache(params, 1000, 1000);
+  cache.start();
+  cache.stop();
+  cache.start();
+  setTimeout(() => {
+    expect(clearTimeoutSpy).toHaveBeenCalledTimes(2);
+    expect(setTimeoutSpy.mock.calls.length).toBeGreaterThanOrEqual(12);
+    expect(pollSpy.mock.calls.length).toBeGreaterThanOrEqual(10);
+    expect(cache.getSessionInformation()).toEqual(expected);   
+    done();
+  }, 5000);
+});
