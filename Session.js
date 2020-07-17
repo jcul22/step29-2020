5e99b54dcee190e42ceddb23fa715cf947475404
jcul22 <jculbreath@google.com>
2020-07-17 17:08:27 +0000

    Added setters and fromObject function to both files respectively.

--- Session.js
+++ Session.js
@@ -1,6 +1,7 @@
 class Session { 
-// To be utilized when unwrapping JSON from the future servlets. Will be on the client side and be used as a buffer  
-// for client to server information needed for other components of Virtual Movie Night.
+// To be utilized when unwrapping JSON from the future servlets. Will be on the 
+// client side and be used as a buffer for client to server information needed  
+// for other components of Virtual Movie Night.
  
     constructor() {
         const expected = {
@@ -8,13 +9,33 @@ class Session {
             screenNameOfController: 'chris',
             ipOfVM: '01',
             listOfAttendees: ['chris', 'bryan']
-            };
+        };
         this.sessionId = expected.sessionId;
         this.ipOfVM = expected.ipOfVM;
         this.listOfAttendees = expected.listOfAttendees;
         this.screenNameOfController = expected.screenNameOfController; 
     }
     
+    setSessionId(id) { 
+        this.sessionId = id;
+        return this.sessionId;
+    }
+
+    setIpOfVM(ip) {
+        this.ipOfVM = ip;
+        return ip;
+    }
+
+    setListOfAttendees(attendees) {
+        this.listOfAttendees = attendees;
+        return attendees;
+    }
+
+    setScreenNameOfController(controller){
+        this.screenNameOfController = controller;
+        return controller;
+    }
+
     getSessionId() {
         return this.sessionId;
     } 
@@ -29,6 +50,15 @@ class Session {
     
     getScreenNameOfController(){
        return this.screenNameOfController; 
+    }
+    
+    static fromSession(session) { 
+        sess = new Session(); 
+        setSessionId();
+        setIpOfVM();
+        setListOfAttendees();
+        setScreenNameOfController();
+        return sess;
     }
     
 }
--- User.js
+++ User.js
@@ -1,18 +1,30 @@
-//The User class can be utililized to keep certain information of the user, a class for the user needed to be 
-// created in addition to the session class. The test for this JS class file utilizes Jest.IO
+// The User class can be utililized to keep certain information of the user, 
+// a class for the user needed to be created in addition to the session class. 
+// The test for this JS class file utilizes Jest.IO
 
 class User {
     constructor() {
-        const expected = {
-            sessionId: 'JTC6',
-            controllerStatus = true,
-            userIp = "129.23.321.12"
-        };
         this.sessionId = expected.sessionId;
         this.controllerStatus = expected.controllerStatus;
         this.userIp = expected.userIp;
     
     }
+        
+    setSessionId(id) { 
+        this.sessionId = id;
+        return this.sessionId;
+    }
+
+    setControllerStatus(status) {
+        this.controllerStatus = status;
+        return this.controllerStatus;
+    }
+
+    setUserIp(ip) {
+        this.userIp = ip;
+        return this.UserIp;
+    }
+
     getScreenName() {
         return this.screenName;
     }
@@ -24,7 +36,13 @@ class User {
     getUserIp() {
         return this.userIp;
     }
-    
+    static fromUser() { 
+        newUser = new User();
+        setSessionId();
+        setUserIp();
+        setControllerStatus();
+        return newUser;
+    }
     
 } 
 export { User };
