418c8b7e1da695e712171339a6001ff39551e179
ckdesir <christopher.desir01@gmail.com>
2020-07-23 18:02:00 -0400

    Removes private and non-existing tests

--- src/main/webapp/scripts/serverclient.test.js
+++ src/main/webapp/scripts/serverclient.test.js
@@ -1,23 +1,5 @@
 import { ServerClient } from './serverclient';
 
-test('We can check if correct errors are thrown - start', () => {
-  try {
-    const client = new ServerClient();
-    client.start();
-  } catch (e) {
-    expect(e.message).toBe('Unimplemented');
-  }
-});
-
-test('We can check if correct errors are thrown - stop', () => {
-  try {
-    const client = new ServerClient();
-    client.stop();
-  } catch (e) {
-    expect(e.message).toBe('Unimplemented');
-  }
-});
-
 test('We can check if correct errors are thrown - passController', () => {
   try {
     const client = new ServerClient();
