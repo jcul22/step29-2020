edf5e4060a94427f5e9e071dea7aa27054e16aa8
ckdesir <christopher.desir01@gmail.com>
2020-07-26 14:07:35 -0400

    Updates style and in-session webpage

--- src/main/webapp/scripts/session-script.js
+++ src/main/webapp/scripts/session-script.js
@@ -1,16 +1,16 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * This waits until the webpage loads and then it calls the
+ * anonymous function, which calls main.
+ */
+window.onload = function() { main(); }
+
+/**
+ * function main() connects the client to a session and begins many of
+ * the behind the scenes operations, like caching.
+ */
+function main() {
+  addOnClickToElements();
+}
 
 /**
  * Adds an onclick event listener to some of the elements on the
@@ -59,4 +59,3 @@ function copyTextToClipboard(element) {
 }
 
 export { openSessionInfo, closeDisplay, copyTextToClipboard };
-
