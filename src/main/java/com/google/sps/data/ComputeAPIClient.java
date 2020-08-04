10ff1ba9ed0df7396176025ab72f13394f004ba3
Taniecer <taniecer@gmail.com>
2020-08-04 15:48:38 +0000

    Adds custom image and address comments

--- src/main/java/com/google/sps/data/ComputeAPIClient.java
+++ src/main/java/com/google/sps/data/ComputeAPIClient.java
@@ -37,20 +37,20 @@ import com.google.auth.oauth2.GoogleCredentials;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.lang.reflect.*;
 
-
 /** Class that creates, stops and deletes VM instances. */
 public class ComputeAPIClient implements ComputeAPIClientInterface {
   private static final String APPLICATION_NAME = "VMN";
-  private static final String PROJECT_ID = "taniecer-step-2020";
+  private static final String PROJECT_ID = "cdesir-step-2020";
   private static final String ZONE_NAME = "us-central1-f";
   private static final String SOURCE_IMAGE_PREFIX =
     "https://www.googleapis.com/compute/v1/projects/";
   private static final String SOURCE_IMAGE_PATH =
-    "eip-images/global/images/ubuntu-1604-lts-drawfork-v20200208";
+    "cdesir-step-2020/global/images/vmn-image";
   private static final String NETWORK_INTERFACE_CONFIG = "ONE_TO_ONE_NAT";
   private static final String NETWORK_ACCESS_CONFIG = "External NAT";
   private static final long OPERATION_TIMEOUT_MILLIS = 60 * 1000;
@@ -59,154 +59,90 @@ public class ComputeAPIClient implements ComputeAPIClientInterface {
     JacksonFactory.getDefaultInstance();
   private static GoogleCredentials credential;
   private Compute compute;
+  private Operation op;
   private final String OPERATION_STATUS_DONE = "DONE";
-
-  /**
-   * Example of how to use computeManager
-   * ComputeAPIClientInterface example = new ComputeAPIClient();
-   * example.computeManager("createInstance", "vm1", Optional.of("12345"));
-   * example.computeManager("deleteInstance","testvm", null);
-   */
-  public void computeManager(String operation, String instanceName,
-    Optional<String> sessionId) {
-      try {
-        httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        credential = GoogleCredentials.getApplicationDefault();
-        if (credential.createScopedRequired()) {
-          List<String> scopes = new ArrayList<>();
-          scopes.add(ComputeScopes.DEVSTORAGE_FULL_CONTROL);
-          scopes.add(ComputeScopes.COMPUTE);
-          credential = credential.createScoped(scopes);
-        }
-        HttpRequestInitializer requestInitializer = 
-          new HttpCredentialsAdapter(credential);
-        // Create Compute Engine object.
-        compute = new Compute.Builder
-          (httpTransport, JSON_FACTORY, requestInitializer)
-          .setApplicationName(APPLICATION_NAME)
-          .build();
-        Operation op = null;
-        if (operation == "createInstance") {
-          op = createInstance(instanceName, sessionId.get());
-        }
-        else if (operation == "deleteInstance") {
-          op = deleteInstance(instanceName);
-        }
-        else if (operation == "stopInstance") {
-          op = stopInstance(instanceName);
-        }
-        else if (operation == "restartInstance") {
-          op = restartInstance(instanceName, sessionId.get());
-        }
-        else {
-          System.out.println("Operation not found!");
-        }
-        // Call Compute Engine API operation and poll for operation completion 
-        // status
-        System.out.println("Waiting for operation completion...");
-        Operation.Error error = blockUntilComplete
-          (compute, op, OPERATION_TIMEOUT_MILLIS);
-        if (error == null) {
-          System.out.println("Success!");
-        } else {
-          System.out.println(error.toPrettyString());
-        }
-      } catch (IOException e) {
-          System.out.println("Got error!");
-          System.err.println(e.getMessage());
-      } catch (Throwable t) {
-          t.printStackTrace();
-        }
-    }
+  
+  private void createEnvironment() {
+    try {
+      httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+      credential = GoogleCredentials.getApplicationDefault();
+      if (credential.createScopedRequired()) {
+        List<String> scopes = new ArrayList<>();
+        scopes.add(ComputeScopes.DEVSTORAGE_FULL_CONTROL);
+        scopes.add(ComputeScopes.COMPUTE);
+        credential = credential.createScoped(scopes);
+      }
+      HttpRequestInitializer requestInitializer = 
+        new HttpCredentialsAdapter(credential);
+      // Create Compute Engine object.
+      compute = new Compute.Builder
+        (httpTransport, JSON_FACTORY, requestInitializer)
+        .setApplicationName(APPLICATION_NAME)
+        .build();
+    } catch (IOException e) {
+        System.out.println("Got error!");
+        System.err.println(e.getMessage());
+    } catch (Throwable t) {
+        t.printStackTrace();
+      }
+  }
     
   // [START create_instances]
-  public Operation createInstance(String instanceName, String sessionId)
+  public void createInstance(String instanceName, String vncServerPassword)
     throws IOException {
+      createEnvironment();
       Instance instance = new Instance();
       instance.setName(instanceName);
       instance.setMachineType(String.format(
-        "projects/taniecer-step-2020/zones/us-central1-f/machineTypes/n1-standard-1",
+        "projects/cdesir-step-2020/zones/us-central1-f/machineTypes/n1-standard-1",
         PROJECT_ID, ZONE_NAME));
-      NetworkInterface ifc = new NetworkInterface();
-      ifc.setNetwork(String.format(
-        "https://www.googleapis.com/compute/v1/projects/%s/global/networks/default",
-        PROJECT_ID));
-      List<AccessConfig> configs = new ArrayList<>();
-      AccessConfig config = new AccessConfig();
-      config.setType(NETWORK_INTERFACE_CONFIG);
-      config.setName(NETWORK_ACCESS_CONFIG);
-      configs.add(config);
-      ifc.setAccessConfigs(configs);
-      instance.setNetworkInterfaces(Collections.singletonList(ifc));
-      AttachedDisk disk = new AttachedDisk();
-      disk.setBoot(true);
-      disk.setAutoDelete(true);
-      disk.setType("PERSISTENT");
-      AttachedDiskInitializeParams params = new AttachedDiskInitializeParams();
-      params.setDiskName(instanceName);
-      params.setSourceImage(SOURCE_IMAGE_PREFIX + SOURCE_IMAGE_PATH);
-      params.setDiskType(String.format(
-        "projects/taniecer-step-2020/zones/us-central1-a/diskTypes/pd-standard",
-        PROJECT_ID, ZONE_NAME));
-      disk.setInitializeParams(params);
-      instance.setDisks(Collections.singletonList(disk));
-      // Adds startup script to be used by VM Instance.
-      Metadata meta = new Metadata();
-      Metadata.Items item = new Metadata.Items();
-      item.setKey("startup-script-url");
-      item.setValue(String.format("gs://%s/starter_script.sh", PROJECT_ID));
-      meta.setItems(Collections.singletonList(item));
-      Metadata.Items item2 = new Metadata.Items();
-      item2.setKey("session-id");
-      item2.setValue(sessionId);
-      meta.setItems(Collections.singletonList(item2));
-      instance.setMetadata(meta);
-      ServiceAccount account = new ServiceAccount();
-      account.setEmail("default");
-      List<String> scopes = new ArrayList<>();
-      scopes.add("https://www.googleapis.com/auth/devstorage.full_control");
-      scopes.add("https://www.googleapis.com/auth/compute");
-      account.setScopes(scopes);
-      instance.setServiceAccounts(Collections.singletonList(account));
+      setUpNetworkInterace(instance);
+      SetUpDisk(instance);
+      SetUpMetadata(instance, vncServerPassword);
+      SetUpServiceAccounts(instance);
       System.out.println(instance.toPrettyString());
       Compute.Instances.Insert create = 
         compute.instances().insert(PROJECT_ID, ZONE_NAME, instance);
-      return create.execute();
+      op = create.execute();
+      waitForOperationCompletion();
     } 
 
-  public Operation stopInstance(String instanceName) throws Exception {
+  public void stopInstance(String instanceName) throws Exception {
+    createEnvironment();
     Compute.Instances.Stop stop =
       compute.instances().stop(PROJECT_ID, ZONE_NAME, instanceName);
-    return stop.execute();
+    op = stop.execute();
+    waitForOperationCompletion();
   }
 
-  public Operation restartInstance(String instanceName, String sessionId) 
+  public void restartInstance(String instanceName, String vncServerPassword) 
     throws Exception {
-      // Adds startup script to be used by VM Instance.
-      Instance instance = new Instance();
-      instance.setName(instanceName);
-      Metadata meta = new Metadata();
-      Metadata.Items item = new Metadata.Items();
-      item.setKey("startup-script-url");
-      item.setValue(String.format("gs://%s/starter_script.sh", PROJECT_ID));
-      Metadata.Items item2 = new Metadata.Items();
-      item2.setKey("session-id");
-      item2.setValue(sessionId);
-      meta.setItems(Collections.singletonList(item));
-      meta.setItems(Collections.singletonList(item2));
-      instance.setMetadata(meta);
+      createEnvironment();
+      Compute.Instances.Get getInstance = 
+        compute.instances().get(PROJECT_ID, ZONE_NAME, instanceName);
+      Instance instance = getInstance.execute();
+      String getFingerPrints = instance.getMetadata().getFingerprint();
+      Metadata metadata = createMetadata(vncServerPassword);
+      metadata.setFingerprint(getFingerPrints);
+      Compute.Instances.SetMetadata setMeta = 
+        compute.instances().setMetadata
+        (PROJECT_ID, ZONE_NAME, instanceName, metadata);
+      setMeta.execute();
       Compute.Instances.Start restart =
         compute.instances().start(PROJECT_ID, ZONE_NAME, instanceName);
-      return restart.execute();
+      op = restart.execute();
+      waitForOperationCompletion();
     }
 
-  public Operation deleteInstance(String instanceName) throws Exception {
+  public void deleteInstance(String instanceName) throws Exception {
+    createEnvironment();
     Compute.Instances.Delete delete =
       compute.instances().delete(PROJECT_ID, ZONE_NAME, instanceName);
-    return delete.execute();
+    op = delete.execute();
+    waitForOperationCompletion();
   }
 
-  public Operation.Error blockUntilComplete(
+  private Operation.Error blockUntilComplete(
     Compute compute, Operation operation, long timeout) throws Exception {
       long start = System.currentTimeMillis();
       final long pollIntervalMs = 5 * 1000;
@@ -240,4 +176,82 @@ public class ComputeAPIClient implements ComputeAPIClientInterface {
       }
       return operation == null ? null : operation.getError();
     }
+
+  private void setUpNetworkInterace(Instance instance) {
+    NetworkInterface ifc = new NetworkInterface();
+    ifc.setNetwork(String.format(
+    "https://www.googleapis.com/compute/v1/projects/%s/global/networks/default",
+    PROJECT_ID));
+    List configs = new ArrayList<>();
+    AccessConfig config = new AccessConfig();
+    config.setType(NETWORK_INTERFACE_CONFIG);
+    config.setName(NETWORK_ACCESS_CONFIG);
+    configs.add(config);
+    ifc.setAccessConfigs(configs);
+    instance.setNetworkInterfaces(Collections.singletonList(ifc));
+  }
+
+  private void SetUpDisk(Instance instance) {
+    AttachedDisk disk = new AttachedDisk();
+    disk.setBoot(true);
+    disk.setAutoDelete(true);
+    disk.setType("PERSISTENT");
+    AttachedDiskInitializeParams params = new AttachedDiskInitializeParams();
+    params.setDiskName(instance.getName());
+    params.setSourceImage(SOURCE_IMAGE_PREFIX + SOURCE_IMAGE_PATH);
+    params.setDiskType(String.format(
+      "projects/cdesir-step-2020/zones/us-central1-a/diskTypes/pd-standard",
+      PROJECT_ID, ZONE_NAME));
+    disk.setInitializeParams(params);
+    instance.setDisks(Collections.singletonList(disk));
+  }
+  
+  private void SetUpMetadata(Instance instance, String vncServerPassword) {
+    instance.setMetadata(createMetadata(vncServerPassword));
+  }
+
+  private Metadata createMetadata(String vncServerPassword) {
+    Metadata meta = new Metadata();
+    Metadata.Items item = new Metadata.Items();
+    item.setKey("startup-script-url");
+    item.setValue(String.format("gs://%s/starter_script.sh", PROJECT_ID));
+    Metadata.Items item2 = new Metadata.Items();
+    item2.setKey("vnc-server-password");
+    item2.setValue(vncServerPassword);
+    meta.setItems(Arrays.asList(item, item2 )); 
+    return meta;
+  }
+
+  private void SetUpServiceAccounts(Instance instance) {
+    ServiceAccount account = new ServiceAccount();
+    account.setEmail("default");
+    List<String> scopes = new ArrayList<>();
+    scopes.add("https://www.googleapis.com/auth/devstorage.full_control");
+    scopes.add("https://www.googleapis.com/auth/compute");
+    account.setScopes(scopes);
+    instance.setServiceAccounts(Collections.singletonList(account));
+  }
+
+  /**
+   * Call Compute Engine API operation and poll for operation completion
+   * status. 
+   */
+  private void waitForOperationCompletion() {
+    try {
+    
+    System.out.println("Waiting for operation completion...");
+    Operation.Error error = blockUntilComplete
+      (compute, op, OPERATION_TIMEOUT_MILLIS);
+    if (error == null) {
+      System.out.println("Success!");
+    } else {
+      System.out.println(error.toPrettyString());
+    }
+  }catch (IOException e) {
+        System.out.println("Got error!");
+        System.err.println(e.getMessage());
+    } catch (Throwable t) {
+        t.printStackTrace();
+      }
+  }
 }
--- src/main/java/com/google/sps/data/ComputeAPIClientInterface.java
+++ src/main/java/com/google/sps/data/ComputeAPIClientInterface.java
@@ -7,51 +7,33 @@ import java.util.Optional;
 /** Interface for the ComputerAPIClient class. */
 public interface ComputeAPIClientInterface {
   /** 
-   * Creates an environment, calls each operation and tracks to see if they 
-   * are executed.
-   * @param {String} operation - the name of the method that should be called.
-   * @param {String} instanceName - the name of the instance being operated on.
-   * @param {Optional<String>} sessionId - the session id associated with
-   *     this instance. 
-   */
-  void computeManager(String operation, String instanceName, 
-    Optional<String> sessionId);
-
-  /** 
    * Creates a new Instance.
    * @param {String} instanceName - the name of the new Instance.
-   * @param {String} sessionId - the session associated with this instance.
-   * @return Operation object that can be used to wait for operation completion
-   *    and to check whether operation failed or succeeded
+   * @param {String} vncServerPassword - the password needed to access 
+   *    to vnc server.
    */
-  Operation createInstance(String instanceName, String sessionId) 
+  void createInstance(String instanceName, String vncServerPassword) 
     throws IOException;
 
   /**
    * Stops an Instance.
    * @param {String} instanceName - the name of the instance that will 
    *    be stopped.
-   * @return Operation object that can be used to wait for operation completion
-   *    and to check whether operation failed or succeeded
    */
-  Operation stopInstance(String instanceName) throws Exception;
+  void stopInstance(String instanceName) throws Exception;
 
   /** 
    * Restart an Instance that was stopped.
    * @param {String} instanceName - the name of the Instance.
    * @param {String} sessionId - the session associated with this instance. 
-   * @return Operation object that can be used to wait for operation completion
-   *    and to check whether operation failed or succeeded
    */
-  Operation restartInstance(String instanceName, String sessionId) 
+  void restartInstance(String instanceName, String vncServerPassword) 
     throws Exception;
 
   /**
    * Deletes an Instance.
    * @param {String} instanceName - the name of the instance that will 
    *    be deleted.
-   * @return Operation object that can be used to wait for operation completion
-   *    and to check whether operation failed or succeeded
    */
-  Operation deleteInstance(String instanceName) throws Exception;
+  void deleteInstance(String instanceName) throws Exception;
 }
